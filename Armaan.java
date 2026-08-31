e35df60ac714fa04ac7052cf8822730f0245ac2a
amali052024-tech <amali052024@vt.edu>
2026-08-31 16:01:29 -0400

    Update Armaan.java

--- Armaan.java
+++ Armaan.java
@@ -1,6 +1,5 @@
 public class Armaan {
     public static void main(String[] args) {
-        System.out   .println("Armaan is on the team.");
+        System.out.println("Armaan is on the team.");
     }
 }
-hs
