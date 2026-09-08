29560647d3e9288cb779f0a7020250a109b851b5
Jonathan <ejonathan25@vt.edu>
2026-09-08 12:12:40 -0400

    Update Jonathan.java

--- Jonathan.java
+++ Jonathan.java
@@ -1,5 +1,5 @@
 public class Jonathan {
     public static void main(String[] args) {
-        System.out.println("Jonathan Elliott is on the team.");
+        System.out.println("Jonathan is on the team.");
     }
 }
