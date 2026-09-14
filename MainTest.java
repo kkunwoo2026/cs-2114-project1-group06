6168102349fa238b3efd88fb4c850ac957cb9482
amali052024-tech <amali052024@vt.edu>
2026-09-14 15:23:45 -0400

    Create MainTest.java

---
+++ MainTest.java
@@ -0,0 +1,53 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those 
+// who do.
+// -- Armaan Ali (amali052024)
+//
+// LLM Statement:
+// I have not used any assistance for the assignment beyond course 
+// resources and staff.
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author Armaan
+ *  @version Sep 14, 2026
+ */
+public class MainTest
+{
+
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp()
+        throws Exception
+    {
+    }
+    //~ Fields ................................................................
+
+
+    /**
+     * Test method for {@link Main#Main()}.
+     */
+    @Test
+    public void testMain()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
