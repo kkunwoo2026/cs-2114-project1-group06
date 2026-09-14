9876ea8636dc2509cd9495472fb6b3ed57aa7809
amali052024-tech <amali052024@vt.edu>
2026-09-14 15:14:03 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those 
+// who do.
+// -- Armaan Ali (amali052024), Jonathan Elliot (), Kunwoo Kim ()
+//
+// LLM Statement: ******
+// I have not used any assistance for the assignment beyond course 
+// resources and staff.
+
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author Armaan
+ *  @version Sep 14, 2026
+ */
+public class Main
+{
+
+    // ----------------------------------------------------------
+    /**
+     * Create a new Main object.
+     */
+    public Main()
+    {
+        // TODO Auto-generated constructor stub
+    }
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
