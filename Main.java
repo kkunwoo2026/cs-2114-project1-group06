f8810cb0acef340e2da9022b7c76e3d31f45f06a
Jonathan <ejonathan25@vt.edu>
2026-09-14 16:54:08 -0400

    list of prizes i think might work

--- Main.java
+++ Main.java
@@ -19,9 +19,16 @@
  */
 import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 public class Main
 {
+    
+  //~ Fields ................................................................
 
+    //~ Constructors ..........................................................
+    
+    //~Public  Methods ........................................................
     // ----------------------------------------------------------
     /**
      * The main game
@@ -31,6 +38,7 @@ public class Main
         boolean roundRun = true;
         Claw claw = new Claw();
         Scanner userInput = new Scanner(System.in);
+        
         while (roundRun)
         {
             System.out.print("Test: Enter command: ");
@@ -41,17 +49,45 @@ public class Main
                 roundRun = false;
             }
             claw.move(userInputString);
-        }
-        
-        
-        
-        
+        }           
     }
     
+    /**
+     * creates the prizes and the list of them
+     */
+    public List<Prize> prizeListCreation()
+    {
+        List<Prize> prizes = new ArrayList<>();
+        Prize prize1 = new Prize();
+        prize1.setPrize(generateRandomNumber(0, 5), generateRandomNumber(0, 5));
+        prizes.add(prize1);
+        Prize prize2 = new Prize();
+        prize2.setPrize(generateRandomNumber(0, 5), generateRandomNumber(0, 5));
+        prizes.add(prize2);
+        Prize prize3 = new Prize();
+        prize3.setPrize(generateRandomNumber(0, 5), generateRandomNumber(0, 5));
+        prizes.add(prize3);
+        
+        for (int i = 0; i < prizes.size(); i++)
+        {
+            for (int j = i; j < prizes.size(); j++)
+            {
+                while (prizes.get(i).equals(prizes.get(j)))
+                {
+                    prizes.set(j, new Prize());
+                }
+            }
+        }
+        
+        return prizes;
+    }
     
-    //~ Fields ................................................................
-
-    //~ Constructors ..........................................................
-    
-    //~Public  Methods ........................................................
+    /**
+     * generate random number
+     */
+    public int generateRandomNumber(int min, int max)
+    {
+        Random rand =  new Random();
+        return rand.nextInt((max - min) + 1) + min;
+    }
 }
