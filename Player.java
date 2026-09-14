9e04b27976a0e8dfcfc28db379b49fb61f8d36fb
amali052024-tech <amali052024@vt.edu>
2026-09-14 16:45:26 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,35 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those 
+// who do.
+// -- Armaan Ali (amali052024)
+//
+// LLM Statement:
+// I have not used any assistance for the assignment beyond course 
+// resources and staff.
+
+/**
+ *  This is how the player would utilize from their inventory of items.
+ * 
+ *  @author Armaan
+ *  @version Sep 14, 2026
+ */
+public class Player
+{
+
+    // ----------------------------------------------------------
+    /**
+     * Create a new Player object.
+     */
+    public Player()
+    {
+        //~ Fields ................................................................
+
+        //~ Constructors ..........................................................
+
+        //~Public  Methods ........................................................
+    }
+    
+
+}
